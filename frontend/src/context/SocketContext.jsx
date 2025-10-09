141b28fa286c0c3469dc90af63db3a2b4bb69fe2
John Gledhill <johnrg56@gmail.com>
2025-10-09 04:08:19 -0600

    updated Socket Context with current URL

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-yt-4yo4.onrender.com", {
+      const socket = io("https://chat-app-yt-1u8x.onrender.com", {
         query: {
           userId: authUser._id,
         },
